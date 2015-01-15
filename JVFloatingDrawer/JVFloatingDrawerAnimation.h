559234e96123847c6a004d2e33d383ae5f0a2b60
JVillella <julian.villella@hotmail.com>
2015-01-14 22:45:38 -0500

    Declares rotation signatures in animation protocol

--- JVFloatingDrawer/JVFloatingDrawerAnimation.h
+++ JVFloatingDrawer/JVFloatingDrawerAnimation.h
@@ -16,8 +16,8 @@
 /**
  *  Implementations should animate the sideViewController into view.
  *
- *  @param sideViewController   The view controller being presented
- *  @param centerViewController The view controller to move out of the way.
+ *  @param sideView   The side view being presented
+ *  @param centerView The center view to move out of the way
  */
 - (void)presentationAnimationWithSide:(JVFloatingDrawerSide)drawerSide sideView:(UIView *)sideView
                            centerView:(UIView *)centerView completion:(void(^)(BOOL finished))completion;
@@ -25,11 +25,35 @@
 /**
  *  Implementations should animate the sideViewController being dismissed.
  *
- *  @param sideViewController   The view controller being dismissed
- *  @param centerViewController The view controller the should be moved back into place
+ *  @param sideView   The containing side view being dismissed
+ *  @param centerView The containing center view the should be moved back into place
  */
 - (void)dismissAnimationWithSide:(JVFloatingDrawerSide)drawerSide sideView:(UIView *)sideView
                       centerView:(UIView *)centerView completion:(void(^)(BOOL finished))completion;
+
+/**
+ *  Invoked before a rotation begins with the drawer open. Implementations should
+ *  do whatever drawer handling is necessary to maintain the drawer's appearance.
+ *
+ *  @param drawerSide The open side of the drawer
+ *  @param sideView   The containing side view that is open
+ *  @param centerView The containing center view
+ *  @param completion An optional completion block
+ */
+- (void)willRotateOpenDrawerWithOpenSide:(JVFloatingDrawerSide)drawerSide sideView:(UIView *)sideView
+                              centerView:(UIView *)centerView completion:(void(^)(BOOL finished))completion;
+
+/**
+ *  Invoked after a rotation is finished with the drawer open. Implementations should
+ *  do whatever drawer handling is necessary to maintain the drawer's appearance.
+ *
+ *  @param drawerSide The open side of the drawer
+ *  @param sideView   The containing side view that is open
+ *  @param centerView The containing center view
+ *  @param completion An optional completion block
+ */
+- (void)didRotateOpenDrawerWithOpenSide:(JVFloatingDrawerSide)drawerSide sideView:(UIView *)sideView
+                             centerView:(UIView *)centerView completion:(void(^)(BOOL finished))completion;
 
 @end
 
